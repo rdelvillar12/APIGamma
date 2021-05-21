512c06bad6714136f9b8cfd079c689c0cd8846f6
Ricardo Del Villar <rdelvillar@cantilever.com.mx>
2021-05-21 13:30:44 -0500

    Second Commit

--- rutas/GammaReporte.js
+++ rutas/GammaReporte.js
@@ -4,30 +4,35 @@ const axios = require('axios');
 router.get('/:id', async function (req,res){
   const {id} = req.params;
   res.status(200).json({message: 'Estas conectado a la API'})
-  const response = await axios({
-    url: 'http://52.14.152.216:8080/v1/graphql',
-    method: 'post',
-    data:{
-      query: `{
-        device (where: {
-          pkdevice:{
-            _eq:"${id}"
+  try {
+    const response = await axios({
+      url: 'http://52.14.152.216:8080/v1/graphql',
+      method: 'POST',
+      data:{
+        query: `{
+          device (where: {
+            pkdevice:{
+              _eq:"${id}"
+            }
+          }){
+          device_gamma{
+            gabinetes{
+              pkgabinete
+              slatitud
+              slongitud
+              sdireccion
+            }
           }
-        }){
-        device_gamma{
-          gabinetes{
-            pkgabinete
-            slatitud
-            slongitud
-            sdireccion
           }
-        }
-        }
-      }`
-    }
-  })
-  let device = response.data.data.device[0]
-  console.log(device);
+        }`
+      }
+    })
+  
+  let device = response.data.data.device[0].device_gamma[0].gabinetes[0]
+  console.log(device)
+  } catch (error) {
+    let e = error;
+  }
 });
 
 module.exports = router;
